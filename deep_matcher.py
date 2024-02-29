1076364cfcc2e27271a1934a3413774b2c764c97
Wang Haoyi <72964929+Mikehuntisbald@users.noreply.github.com>
2024-02-29 17:18:39 +0800

    Add files via upload

--- deep_matcher.py
+++ deep_matcher.py
@@ -42,7 +42,7 @@ class Correlator:
 
     def callback(self, msg1, msg2):
         flag = rospy.get_param('/flag', False)
-
+        print("frame", int(msg1.header.stamp.to_sec()))
         # 把msg转换成能处理的Detection格式
         model_filename = self.deep_weights #Change it to your directory
         # encoder = generate_detections.create_box_encoder(model_filename)
@@ -83,47 +83,64 @@ class Correlator:
             self.matcher.tracks_1 = [track for track in self.matcher.tracks_1 if track.track_id.item() != 2]
 
         self.matcher.update(detections_2)
-        for track in self.matcher.tracks_1:
-            # if track.is_confirmed() and track.time_since_update > 1: # se o objeto foi confirmado e nao foi atualizado a mais de 1 frame
-            #     continue
-            # print(type(track))
-            bbox = track.to_tlbr()
-            track_id = track.track_id
 
-            xmin, ymin, xmax, ymax = bbox[0], bbox[1], bbox[2], bbox[3]
-            # cv2.rectangle(scene, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
-            # 绘制类别、ID 和 分数
-            # label = f"{class_name} ID: {track_id} Score: {score:.2f}"
-            label = f"ID: {track_id}"
+        # 假设frame是当前处理的帧编号，从msg1.header.stamp.to_sec()获取
+        frame = int(msg1.header.stamp.to_sec())
 
-            font_scale = 1.0  # 字体大小
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            thickness = 2  # 文本线条厚度
+        # 示例文件名，您可以根据需要修改
+        output_filename = "/home/sam/tracking_output.txt"
+        # 打开文件准备写入
+        with open(output_filename, "a") as file:
+            for track in self.matcher.tracks_1:
+                # if track.is_confirmed() and track.time_since_update > 1: # se o objeto foi confirmado e nao foi atualizado a mais de 1 frame
+                #     continue
+                # print(type(track))
+                bbox = track.to_tlbr()
+                track_id = track.track_id
 
-            if track.restored and flag == True:
-                # label = f"ID: {track_id} Retrieved"
-                # cv2.putText(scene, label, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 3)
-                cv2.rectangle(scene, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 255, 255), -1)
+                # 构造KITTI格式的字符串
+                kitti_format_str = f"{frame} {track_id} Car -1 -1 -10 {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]} -1 -1 -1 -1000 -1000 -1000 -10\n"
 
-                text = "Attack Detected"
-                text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-                text_x = scene.shape[1] - text_size[0] - 10  # 距离右边 10 像素
-                text_y = text_size[1] + 10  # 距离顶部 10 像素
-                # 设置字体颜色为红色
-                font_color = (255, 0, 0)
-                # 在图像上绘制文本
-                # cv2.putText(scene, text, (text_x, text_y), font, font_scale, font_color, thickness)
-            else:
-                cv2.putText(scene, label, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 3)
-                cv2.rectangle(scene, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
-                # text = "No Attack"
-                # text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-                # text_x = scene.shape[1] - text_size[0] - 10  # 距离右边 10 像素
-                # text_y = text_size[1] + 10  # 距离顶部 10 像素
-                # # 设置字体颜色为绿色
-                # font_color = (0, 255, 0)
-                # # 在图像上绘制文本
-                # cv2.putText(scene, text, (text_x, text_y), font, font_scale, font_color, thickness)
+                # 写入文件
+                file.write(kitti_format_str)
+
+                xmin, ymin, xmax, ymax = bbox[0], bbox[1], bbox[2], bbox[3]
+                # cv2.rectangle(scene, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+                # 绘制类别、ID 和 分数
+                # label = f"{class_name} ID: {track_id} Score: {score:.2f}"
+                label = f"{track_id}"
+
+                font_scale = 1.0  # 字体大小
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                thickness = 2  # 文本线条厚度
+                # if track.restored and flag == True:
+                if track.restored and flag == False:
+                    # label = f"ID: {track_id} Retrieved"
+                    # cv2.putText(scene, label, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 3)
+                    # cv2.rectangle(scene, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 255, 255), -1)
+                    cv2.putText(scene, label, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    cv2.rectangle(scene, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+                    
+                    text = "Attack Detected"
+                    text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
+                    text_x = scene.shape[1] - text_size[0] - 10  # 距离右边 10 像素
+                    text_y = text_size[1] + 10  # 距离顶部 10 像素
+                    # 设置字体颜色为红色
+                    font_color = (255, 0, 0)
+                    # 在图像上绘制文本
+                    # cv2.putText(scene, text, (text_x, text_y), font, font_scale, font_color, thickness)
+                else:
+                    cv2.putText(scene, label, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                    cv2.rectangle(scene, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+                    # text = "No Attack"
+                    # text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
+                    # text_x = scene.shape[1] - text_size[0] - 10  # 距离右边 10 像素
+                    # text_y = text_size[1] + 10  # 距离顶部 10 像素
+                    # # 设置字体颜色为绿色
+                    # font_color = (0, 255, 0)
+                    # # 在图像上绘制文本
+                    # cv2.putText(scene, text, (text_x, text_y), font, font_scale, font_color, thickness)
+
         self.matcher.tracks_1 = []
         self.matcher.tracks_2 = []
         self._imagepub.publish(self._bridge.cv2_to_imgmsg(scene, 'rgb8'))
